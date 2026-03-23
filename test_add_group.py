93daf9315d6cbd2a5f556e76195ef745c0e817ae
Kristinka <cris.moro1133@gmail.com>
2026-03-23 17:50:24 +0300

    Созданы вспомогательные методы, увеличена читаемость кода

--- test_add_group.py
+++ test_add_group.py
@@ -1,30 +1,34 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest
 
 
-class UntitledTestCase(unittest.TestCase):
+class test_add_group(unittest.TestCase):
     def setUp(self):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    def test_untitled_test_case(self):
+    def test_test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element("name", "user").click()
-        wd.find_element("name", "user").clear()
-        wd.find_element("name", "user").send_keys("admin")
-        wd.find_element("name", "pass").click()
-        wd.find_element("name", "pass").clear()
-        wd.find_element("name", "pass").send_keys("secret")
-        wd.find_element("xpath", "//input[@value='Login']").click()
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.create_group(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element("link text", "Logout").click()
+
+    def return_to_groups_page(self, wd):
         wd.find_element("link text", "groups").click()
+
+    def create_group(self, wd):
+        # init group creation
         wd.find_element("name", "new").click()
+        # fill group firm
         wd.find_element("name", "group_name").click()
         wd.find_element("name", "group_name").clear()
         wd.find_element("name", "group_name").send_keys("new group")
@@ -34,9 +38,23 @@ class UntitledTestCase(unittest.TestCase):
         wd.find_element("name", "group_footer").click()
         wd.find_element("name", "group_footer").clear()
         wd.find_element("name", "group_footer").send_keys("wwwww")
+        # submit group creation
         wd.find_element("name", "submit").click()
+
+    def open_groups_page(self, wd):
         wd.find_element("link text", "groups").click()
-        wd.find_element("link text", "Logout").click()
+
+    def login(self, wd):
+        wd.find_element("name", "user").click()
+        wd.find_element("name", "user").clear()
+        wd.find_element("name", "user").send_keys("admin")
+        wd.find_element("name", "pass").click()
+        wd.find_element("name", "pass").clear()
+        wd.find_element("name", "pass").send_keys("secret")
+        wd.find_element("xpath", "//input[@value='Login']").click()
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
         try:
