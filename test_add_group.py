e6e37cd8a89fa2b9aef199d6f40dc1fee258d945
Kristinka <cris.moro1133@gmail.com>
2026-03-24 14:29:35 +0300

    Добавлена параметризация вспомогательных методов. Добавлен тест-кейс на создание группы с пустыми полями.

--- test_add_group.py
+++ test_add_group.py
@@ -10,12 +10,21 @@ class test_add_group(unittest.TestCase):
         self.wd = webdriver.Firefox()
         self.wd.implicitly_wait(30)
 
-    def test_test_add_group(self):
+    def test_add_group(self):
         wd = self.wd
         self.open_home_page(wd)
-        self.login(wd)
+        self.login(wd, username="admin", password="secret")
         self.open_groups_page(wd)
-        self.create_group(wd)
+        self.create_group(wd, name="new group", header="qqqqq", footer="wwwww")
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def test_add_empty_group(self):
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd, username="admin", password="secret")
+        self.open_groups_page(wd)
+        self.create_group(wd, name="", header="", footer="")
         self.return_to_groups_page(wd)
         self.logout(wd)
 
@@ -25,32 +34,32 @@ class test_add_group(unittest.TestCase):
     def return_to_groups_page(self, wd):
         wd.find_element("link text", "groups").click()
 
-    def create_group(self, wd):
+    def create_group(self, wd, name, header, footer):
         # init group creation
         wd.find_element("name", "new").click()
         # fill group firm
         wd.find_element("name", "group_name").click()
         wd.find_element("name", "group_name").clear()
-        wd.find_element("name", "group_name").send_keys("new group")
+        wd.find_element("name", "group_name").send_keys(name)
         wd.find_element("name", "group_header").click()
         wd.find_element("name", "group_header").clear()
-        wd.find_element("name", "group_header").send_keys("qqqqq")
+        wd.find_element("name", "group_header").send_keys(header)
         wd.find_element("name", "group_footer").click()
         wd.find_element("name", "group_footer").clear()
-        wd.find_element("name", "group_footer").send_keys("wwwww")
+        wd.find_element("name", "group_footer").send_keys(footer)
         # submit group creation
         wd.find_element("name", "submit").click()
 
     def open_groups_page(self, wd):
         wd.find_element("link text", "groups").click()
 
-    def login(self, wd):
+    def login(self, wd, username, password):
         wd.find_element("name", "user").click()
         wd.find_element("name", "user").clear()
-        wd.find_element("name", "user").send_keys("admin")
+        wd.find_element("name", "user").send_keys(username)
         wd.find_element("name", "pass").click()
         wd.find_element("name", "pass").clear()
-        wd.find_element("name", "pass").send_keys("secret")
+        wd.find_element("name", "pass").send_keys(password)
         wd.find_element("xpath", "//input[@value='Login']").click()
 
     def open_home_page(self, wd):
