b72b276712ad8393e11376af74c9d3acc468f08e
Kristinka <cris.moro1133@gmail.com>
2026-03-25 18:32:35 +0300

    Добавлена фикстура для автотеста создания контакта.

--- application.py
+++ application.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.support.select import Select
 
 
 class Application:
@@ -7,51 +8,96 @@ class Application:
         self.wd = webdriver.Firefox()  # инициализация драйвера
         self.wd.implicitly_wait(30)
 
-        def logout(self):
-            wd = self.wd
-            wd.find_element("link text", "Logout").click()
+    def logout(self):
+        wd = self.wd
+        wd.find_element("link text", "Logout").click()
 
-        def return_to_groups_page(self):
-            wd = self.wd
-            wd.find_element("link text", "groups").click()
+    def return_to_groups_page(self):
+        wd = self.wd
+        wd.find_element("link text", "groups").click()
 
-        def create_group(self, group):
-            wd = self.wd
-            self.open_groups_page()
-            # init group creation
-            wd.find_element("name", "new").click()
-            # fill group firm
-            wd.find_element("name", "group_name").click()
-            wd.find_element("name", "group_name").clear()
-            wd.find_element("name", "group_name").send_keys(group.name)
-            wd.find_element("name", "group_header").click()
-            wd.find_element("name", "group_header").clear()
-            wd.find_element("name", "group_header").send_keys(group.header)
-            wd.find_element("name", "group_footer").click()
-            wd.find_element("name", "group_footer").clear()
-            wd.find_element("name", "group_footer").send_keys(group.footer)
-            # submit group creation
-            wd.find_element("name", "submit").click()
-            self.return_to_groups_page()
+    def create_group(self, group):
+        wd = self.wd
+        self.open_groups_page()
+        # init group creation
+        wd.find_element("name", "new").click()
+        # fill group firm
+        wd.find_element("name", "group_name").click()
+        wd.find_element("name", "group_name").clear()
+        wd.find_element("name", "group_name").send_keys(group.name)
+        wd.find_element("name", "group_header").click()
+        wd.find_element("name", "group_header").clear()
+        wd.find_element("name", "group_header").send_keys(group.header)
+        wd.find_element("name", "group_footer").click()
+        wd.find_element("name", "group_footer").clear()
+        wd.find_element("name", "group_footer").send_keys(group.footer)
+        # submit group creation
+        wd.find_element("name", "submit").click()
+        self.return_to_groups_page()
 
-        def open_groups_page(self):
-            wd = self.wd
-            wd.find_element("link text", "groups").click()
+    def open_groups_page(self):
+        wd = self.wd
+        wd.find_element("link text", "groups").click()
 
-        def login(self, username, password):
-            wd = self.wd
-            self.open_home_page()
-            wd.find_element("name", "user").click()
-            wd.find_element("name", "user").clear()
-            wd.find_element("name", "user").send_keys(username)
-            wd.find_element("name", "pass").click()
-            wd.find_element("name", "pass").clear()
-            wd.find_element("name", "pass").send_keys(password)
-            wd.find_element("xpath", "//input[@value='Login']").click()
+    def login(self, username, password):
+        wd = self.wd
+        self.open_home_page()
+        wd.find_element("name", "user").click()
+        wd.find_element("name", "user").clear()
+        wd.find_element("name", "user").send_keys(username)
+        wd.find_element("name", "pass").click()
+        wd.find_element("name", "pass").clear()
+        wd.find_element("name", "pass").send_keys(password)
+        wd.find_element("xpath", "//input[@value='Login']").click()
 
-        def open_home_page(self):
-            wd = self.wd
-            wd.get("http://localhost/addressbook/")
+    def open_home_page(self):
+        wd = self.wd
+        wd.get("http://localhost/addressbook/")
 
-        def destroy(self):
-            self.wd.quit()
+    def destroy(self):
+        self.wd.quit()
+
+    def create_contact(self, contact):
+        wd = self.wd
+        self.open_add_contact_page()
+        # fill contact name
+        wd.find_element("name", "firstname").click()
+        wd.find_element("name", "firstname").clear()
+        wd.find_element("name", "firstname").send_keys(contact.firstname)
+        # fill contact middlename
+        wd.find_element("name", "middlename").click()
+        wd.find_element("name", "middlename").clear()
+        wd.find_element("name", "middlename").send_keys(contact.middlename)
+        # fill contact lastname
+        wd.find_element("name", "lastname").click()
+        wd.find_element("name", "lastname").clear()
+        wd.find_element("name", "lastname").send_keys(contact.lastname)
+        # fill address
+        wd.find_element("name", "address").click()
+        wd.find_element("name", "address").clear()
+        wd.find_element("name", "address").send_keys(contact.address)
+        # fill e-mail
+        wd.find_element("name", "email").click()
+        wd.find_element("name", "email").clear()
+        wd.find_element("name", "email").send_keys(contact.email)
+        # fill birthday
+        wd.find_element("name", "bday").click()
+        Select(wd.find_element("name", "bday")).select_by_visible_text(contact.bday)
+        # wd.find_element("xpath", "//option[@value='1']").click()
+        wd.find_element("name", "bmonth").click()
+        Select(wd.find_element("name", "bmonth")).select_by_visible_text(contact.bmonth)
+        # wd.find_element("xpath", "//option[@value='January']").click()
+        wd.find_element("name", "byear").click()
+        wd.find_element("name", "byear").clear()
+        wd.find_element("name", "byear").send_keys(contact.byear)
+        # submit contact creation
+        wd.find_element("name", "theform").submit()
+        self.return_to_home_page()
+
+    def open_add_contact_page(self):
+        wd = self.wd
+        wd.find_element("link text", "add new").click()
+
+    def return_to_home_page(self):
+        wd = self.wd
+        wd.find_element("link text", "home").click()
