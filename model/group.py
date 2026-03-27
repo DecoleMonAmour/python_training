2734cdc8fad08387a57b498438b062df409057b5
Kristinka <cris.moro1133@gmail.com>
2026-03-27 18:08:58 +0300

    Доработан метод редактирования группы - теперь можно редактировать любое указанное поле.

--- model/group.py
+++ model/group.py
@@ -1,8 +1,6 @@
-__author__ = 'Kristinka'
-
 class Group:
 
-    def __init__(self, name, header, footer):
+    def __init__(self, name=None, header=None, footer=None):
         self.name = name
         self.header = header
         self.footer = footer
--- test/test_edit_group.py
+++
@@ -1,7 +0,0 @@
-from model.group import Group
-
-
-def test_edit_first_group(app):
-    app.session.login(username="admin", password="secret")
-    app.group.edit_first_group(Group(name='Edit group', header="Edit header", footer="Edit footer"))
-    app.session.logout()
